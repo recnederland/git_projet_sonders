8422535738b89254002dcb75e2a229868b1a7cac
Recep <recbay@gmail.com>
2020-08-16 22:00:02 +0200

    master branch son ders selam

---
+++ src/git_project_sonders/SonDersTekrar.java
@@ -0,0 +1,10 @@
+package git_project_sonders;
+
+public class SonDersTekrar {
+
+	public static void main(String[] args) {
+		System.out.println("21:55 master brach, Son github dersinden selamlar...");
+
+	}
+
+}
